07945c654898c38b53537ebc2921fe4ca928216f
Jonathan Heek <heekjonathan@gmail.com>
2016-06-10 20:03:00 +0200

    scene

--- app/index.js
+++ app/index.js
@@ -1,8 +1,8 @@
-console.log('Hello World 123');
+import THREE from 'three';
 
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-class Foo {
-  constructor() {
-    
-  }
-}
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
