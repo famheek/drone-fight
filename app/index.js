41cb8f339534c57506c26ccca4756bdf877a3a08
Jonathan Heek <heekjonathan@gmail.com>
2016-06-14 20:06:16 +0200

    update index

--- app/index.js
+++ app/index.js
@@ -1,51 +1,36 @@
 import THREE from 'three';
-import './STLLoader';
-import './AMFLoader';
+import createPlane from './models/plane';
 
 window.THREE = THREE;
 
 var scene = window.scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var camera = window.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-var loader = new THREE.STLLoader();
-
-let plane;
-
-// loader.load('assets/plane.stl', function ( geometry ) {
-//   scene.add( plane = new THREE.Mesh( geometry, new THREE.MeshPhongMaterial() ) );
-//   window.plane = plane;
-//   plane.scale.setScalar(10);
-//   plane.rotation.z = Math.PI / 2;
-//   plane.rotation.x = Math.PI / 2;
-//   render();
-// });
-
-var loader = new THREE.AMFLoader();
-loader.load('assets/plane.amf', function(objecttree) {
-  scene.add(plane = objecttree);
-  window.plane = plane;
-  plane.scale.setScalar(1);
-  // plane.rotation.z = Math.PI / 2;
-  // plane.rotation.x = Math.PI / 2;
-  render();
-});
+var light = new THREE.PointLight( 0xffffffff, 1, 100 );
+light.position.set( 0, 50, 20 );
+scene.add( light );
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 
-camera.position.z = 10;
+let plane = window.plane = createPlane();
+scene.add(plane);
 
-var light = new THREE.PointLight( 0xffffffff, 1, 100 );
-light.position.set( 0, 50, 20 );
-scene.add( light );
-
+render();
 
 function render() {
-  plane.rotation.x += 0.01;
-
-  renderer.render( scene, camera );
-
   requestAnimationFrame( render );
+  update();
+  renderer.render( scene, camera );
+}
+
+function update() {
+  plane.getWorldPosition(camera.position);
+  camera.position.z = 10;
+
+  camera.lookAt(plane);
+
+  
 }
